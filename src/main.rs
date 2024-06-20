3f351c534db2ce2611ae36908ef24bb71db23dde
aarol <aarol@iki.fi>
2024-06-20 18:27:36 +0300

    faster rust impl from C reference

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
-use std::ops::Mul;
+use std::ops::{AddAssign, Mul, MulAssign};
 
 use rug::{
-    ops::{CompleteRound, Pow},
-    Complete, Float, Integer,
+    ops::{CompleteRound, NegAssign, PowAssign},
+    Assign, Complete, Float, Integer,
 };
 
-use rayon::{
-    self,
-    iter::{IntoParallelIterator, ParallelIterator},
-};
+const A: u32 = 13591409;
+const B: u32 = 545140134;
+const C: u32 = 640320;
+const D: u32 = 12;
+
+const DIGITS_PER_ITER: f64 = 14.1816474627254776555;
+const BITS_PER_DIGIT: f64 = 3.32192809488736234787;
+
 fn main() {
     let digits: u32 = std::env::args()
         .nth(1)
@@ -18,44 +22,128 @@ fn main() {
         })
         .unwrap_or(60);
 
-    chudnovsky(digits);
+    let terms = (digits as f64 / DIGITS_PER_ITER) as usize;
+
+    let mut depth = 0;
+    while 1 << depth < terms {
+        depth += 1;
+    }
+    depth += 1;
+
+    let cores = 1;
+    let mut cores_depth = 0;
+    while 1 << cores_depth < cores {
+        cores_depth += 1;
+    }
+
+    //eprintln!("#terms={terms}, depth={depth}, cores={cores}");
+
+    let mut splitter = CoreSplitter::new(depth);
+
+    splitter.bs(0, terms as u32, 1, cores_depth as usize, 0, 0);
+
+    let default_prec = (digits as f64 * BITS_PER_DIGIT + 16.) as u32;
+
+    /*
+                p*(C/D)*sqrt(C)
+      pi = -----------------
+                (q+A*p)
+    */
+    let p1 = &mut splitter.p_stack[0];
+    let q1 = &mut splitter.q_stack[0];
+    let g1 = &mut splitter.g_stack[0];
+
+    //eprintln!("q={q1}");
+    //eprintln!("p={p1}");
+    //eprintln!("g={g1}");
+
+    q1.add_assign(p1.clone().mul(A));
+    p1.mul_assign(C / D);
+
+    let mut pi = Float::new(default_prec);
+    pi.assign(&*p1);
+
+    let mut qi = Float::new(default_prec);
+    qi.assign(&*q1);
+
+    qi = pi / qi;
+
+    pi = Float::sqrt_u(C).complete(default_prec);
+
+    qi *= pi;
+
+    println!("{}", qi.to_string())
 }
 
-const DIGITS_PER_ITERATION: f32 = 14.1816474627254776555;
-const BITS_PER_DIGIT: f32 = 3.32192809488736234789;
+struct CoreSplitter {
+    p_stack: Vec<Integer>,
+    q_stack: Vec<Integer>,
+    g_stack: Vec<Integer>,
+}
 
-fn chudnovsky(digits: u32) {
-    let iterations = ((digits as f32 / DIGITS_PER_ITERATION) + 1.) as u32;
-    let precision_bits = ((digits as f32 * BITS_PER_DIGIT) + 1.) as u32;
+impl CoreSplitter {
+    fn new(depth: usize) -> Self {
+        Self {
+            p_stack: vec![Integer::new(); depth],
+            g_stack: vec![Integer::new(); depth],
+            q_stack: vec![Integer::new(); depth],
+        }
+    }
 
-    let mut sum: Float = (0..iterations)
-        .into_par_iter()
-        .map(|k| {
-            let three_k = 3 * k;
+    fn bs(&mut self, a: u32, b: u32, g_flag: usize, level: usize, index: usize, top: usize) {
+        //eprintln!(
+        // "bs: a = {a}, b = {b}, gflag = {g_flag} index = {index} level = {level} top = {top}"
+        // );
 
-            let a = Integer::factorial(6 * k).complete();
-            let b = Integer::mul(545140134.into(), k) + 13591409;
-            let c = Integer::factorial(three_k).complete();
+        if b > a && b - a == 1 {
+            /*
+              g(b-1,b) = (6b-5)(2b-1)(6b-1)
+              p(b-1,b) = b^3 * C^3 / 24
+              q(b-1,b) = (-1)^b*g(b-1,b)*(A+Bb).
+            */
+            let p1 = &mut self.p_stack[top];
+            let q1 = &mut self.q_stack[top];
+            let g1 = &mut self.g_stack[top];
 
-            let d = Integer::factorial(k).complete().pow(3);
+            p1.assign(b);
 
-            let mut e = Integer::u_pow_u(640320, three_k).complete();
-            if three_k & 1 == 1 {
-                e = -e;
+            p1.pow_assign(3);
+            *p1 *= (C / 24) * (C / 24);
+            *p1 *= C * 24;
+
+            g1.assign(2 * b - 1);
+            *g1 *= 6 * b - 1;
+            *g1 *= 6 * b - 5;
+
+            q1.assign(b);
+            *q1 *= B;
+            *q1 += A;
+            *q1 *= &*g1;
+
+            if b % 2 == 1 {
+                q1.neg_assign();
             }
+        } else {
+            /*
+            p(a,b) = p(a,m) * p(m,b)
+            g(a,b) = g(a,m) * g(m,b)
+            q(a,b) = q(a,m) * p(m,b) + q(m,b) * g(a,m)
+            */
+            let mid = (a as f32 + (b as f32 - a as f32) * 0.5224) as u32; // tuning parameter
+            self.bs(a, mid, 1, level + 1, index, top);
 
-            let numerator = Float::with_val(precision_bits, a * b);
-            let denominator = Float::with_val(precision_bits, c * d * e);
+            self.bs(mid, b, g_flag, level + 1, index, top + 1);
 
-            numerator / denominator
-        })
-        .reduce(|| Float::new(precision_bits), |stage, a| a + stage);
+            self.p_stack[top] = (&self.p_stack[top] * &self.p_stack[top + 1]).complete();
+            self.q_stack[top] = (&self.q_stack[top] * &self.p_stack[top + 1]).complete();
+            self.q_stack[top + 1] = (&self.q_stack[top + 1] * &self.g_stack[top]).complete();
+            //eprintln!("q_bs_top={}", self.q_stack[top]);
+            self.q_stack[top] = (&self.q_stack[top] + &self.q_stack[top + 1]).into();
+            //eprintln!("q_bs_bot={}", self.q_stack[top]);
 
-    let constant_part = Float::sqrt_u(10005).complete(precision_bits) * 426880;
-
-    sum = 1 / sum * constant_part;
-
-    let pi = sum.to_string();
-
-    println!("{pi}");
+            if g_flag == 1 {
+                self.g_stack[top] = (&self.g_stack[top] * &self.g_stack[top + 1]).complete();
+            }
+        }
+    }
 }
